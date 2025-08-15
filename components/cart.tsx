71e5b577293a503efeac15f7ed8344fd44847b1d
farzeenilyaszargar <farzeeilyaszargarg@gmail.com>
2025-08-16 03:04:51 +0530

    changed cross in cart

--- components/cart.tsx
+++ components/cart.tsx
@@ -5,12 +5,15 @@ export default function Cart({openCart, setOpenCart}:{openCart:boolean; setOpenC
 {
     return(
     <div>
-        <div className={`flex flex-col bg-white opacity-95 w-2/3 md:w-1/4 h-screen text-black fixed top-0 right-0 z-10 transition-all ease-in-out duration-500 ${openCart?"translate-x-0" : "translate-x-full"}`}>
-            <button className={`flex h-20 justify-center items-center w-fit ml-10 text-white text-4xl z-10`} onClick={()=>{setOpenCart(!openCart)}}>
+        <div className={`flex flex-col items-end bg-white opacity-95 w-2/3 md:w-1/3 h-screen text-black fixed top-0 right-0 z-10 transition-all ease-in-out duration-500 ${openCart?"translate-x-0" : "translate-x-full"}`}>
+            <button className={`flex h-20 justify-center items-center w-fit  mr-10 text-white text-4xl z-10`} onClick={()=>{setOpenCart(!openCart)}}>
                 <Image src={'/icons/cross.png'} alt='cross' width={40} height={40} className="invert"/>
             </button> 
-            <div className="flex flex-col justify-around h-1/2 ml-10">
-                <p>akjdhaksjdhakjdhakshdk</p>
+            <div className="flex flex-col items-center min-w-full">
+                <div className="w-3/4 h-50 border rounded ">
+                    akjshdkjashdkahkjdsh
+                </div>
+                
             </div>
             
         </div>
